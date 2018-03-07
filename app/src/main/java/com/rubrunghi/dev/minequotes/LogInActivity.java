9192becf5a0b3acbc423f843537b8df5102d195a
runghi <davidrunghi@gmail.com>
2018-03-07 15:19:48 +0100

    2.oo

--- app/src/main/java/com/rubrunghi/dev/minequotes/LogInActivity.java
+++ app/src/main/java/com/rubrunghi/dev/minequotes/LogInActivity.java
@@ -2,6 +2,7 @@ package com.rubrunghi.dev.minequotes;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
@@ -9,14 +10,26 @@ import android.widget.Button;
  * Created by Admin_David on 28.02.2018.
  */
 
-public class LogInActivity extends AppCompatActivity implements View.OnClickListener {
-    Button login;
+abstract class LogInActivity extends AppCompatActivity implements View.OnClickListener {
+
     protected void onCreate(Bundle savedValues) {
-        login = (Button) findViewById(R.id.logIn_btn);
+        super.onCreate(savedValues);
     }
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main, menu);
+        Button login = (Button) findViewById(R.id.logIn_btn);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+        Button signup = (Button) findViewById(R.id.signUp_btn);
+        signup.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
 
-    @Override
-    public void onClick(View v) {
-
+        }
+        });
+        return true;
     }
 }
